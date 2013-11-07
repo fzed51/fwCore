ba79ffe9f0dfba21fd9c502860550880ced232d7
fzed51 <fzed51@yahoo.fr>
2013-11-07 14:29:22 +0100

    add index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * fwCore
+ * @author Fabien SANCHEZ
+ * @copyright 20132
+ * @version 0.1
+ * =========================================================================
+ * fwCore est un framework léger
+ * Il a été créer pour comprendre le fonctionnement d'un framework
+ * =========================================================================
+ * @license  
+ **/
+
+/* Constantes */
+define('DS', SEPARATOR);
+define('ROOT', dirname(__FILE__));
