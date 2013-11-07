b5101d0d5de1a0ab6e85dc8ca594d890eda806cb
fzed51 <fzed51@yahoo.fr>
2013-11-07 14:51:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,14 +12,18 @@
  **/
 
 // Constantes de chemin
-define('DS', SEPARATOR);
+define('DS', DIRECTORY_SEPARATOR);
+
 define('ROOT', dirname(__FILE__));
+
 define('ROOT_WWW', ROOT . DS . 'www_root');
 define('ROOT_APP', ROOT . DS . 'app');
 define('ROOT_FWK', ROOT . DS . 'fw');
-define('ROOT_MODEL', ROOT_APP . DS . 'MODEL');
+
+define('ROOT_MODEL', ROOT_APP . DS . 'model');
 define('ROOT_VUE', ROOT_APP . DS . 'vue');
 define('ROOT_CONTROLEUR', ROOT_APP . DS . 'controleur');
+
 define('ROOT_JS', ROOT_WWW . DS . 'script');
 define('ROOT_CSS',     ROOT_WWW . DS . 'style');
 define('ROOT_IMAGE', ROOT_WWW . DS . 'image');
