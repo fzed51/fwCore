8f13dd388bbd581e9bea73c53fd8636e1b025690
fzed51 <fzed51@yahoo.fr>
2013-11-12 17:03:01 +0100

    Update Auth class

--- fw/auth.class.php
+++ fw/auth.class.php
@@ -2,6 +2,23 @@
 
 class Auth{
 	
+	static private $needed = null;
+	static public function isNeeded(/*bool*/$needed){
+		self::$needed = $needed;
+	}
+
+	// Singleton
+	static private $handle
+	private function __Construct(){
+
+	}
+	static public function init(){
+		if(is_null(self::$handle)){
+			self::$handle = new Auth();
+		}
+		return self::$handle;
+	}
+
 }
 
 ?>
