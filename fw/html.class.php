a0875eab3ebfe28f0499c6926e2274c3b6fa920a
fzed51 <fzed51@yahoo.fr>
2013-11-12 17:03:16 +0100

    Update Html class

--- fw/html.class.php
+++ fw/html.class.php
@@ -1,7 +1,21 @@
 <?php
 
 class Html{
-	
+	static public function adresse(array $controlAction){
+		return $adresse;
+	}
+	static public function link(/*string*/$libelle, array $controlAction, array $options = array()){
+		$options['raw']=false;
+		return $link;
+	}
+	static public function link(/*string*/$libelle, /*string*/$href, array $options = array()){
+		$defaultOptions = array(
+			"raw" => false;
+			"title" => '';
+			);
+		return $link;
+	}
+
 }
 
 ?>
