30c7eb6edea3bf07908ffd8fe089feda0d91cd88
fzed51 <fzed51@yahoo.fr>
2013-11-12 17:18:19 +0100

    Update Html class

--- fw/html.class.php
+++ fw/html.class.php
@@ -1,18 +1,30 @@
 <?php
 
+/**
+ * Html
+ * 
+ * @package fwCore
+ * @author Fabien SANCHEZ
+ * @copyright 2013
+ * @static
+ * @access public
+ */
+
 class Html{
 	static public function adresse(array $controlAction){
 		return $adresse;
 	}
-	static public function link(/*string*/$libelle, array $controlAction, array $options = array()){
-		$options['raw']=false;
-		return $link;
-	}
-	static public function link(/*string*/$libelle, /*string*/$href, array $options = array()){
+	static public function link(/*string*/$libelle, /*string|array*/$href, array $options = array()){
 		$defaultOptions = array(
-			"raw" => false;
-			"title" => '';
+			"raw" => false,
+			"title" => ''
 			);
+		if( gettype($href) == 'array'){
+			$href = self::adresse($href);
+		}
+		if( gettype($href) != 'string'){
+			throw new InvalidArgumentException("la cible de <Html::link> n'est pas valide!");
+		}
 		return $link;
 	}
 
