866336a17e0e7fb1e5b98b40f9b8a6f0054fb93b
fzed51 <fzed51@yahoo.fr>
2013-11-13 15:13:41 +0100

    Update auth class

--- fw/auth.class.php
+++ fw/auth.class.php
@@ -4,13 +4,39 @@ class Auth extends Singleton{
 	
 	private $_levelNeeded;
 	private $_session;
+	private $_id;
+	private $_level;
+	
+	private function __Construct(){
+		$this->_levelNeeded = 1;
+		$this->_session = Session::getInstance();
+		$this->_id = 0;
+		$this->_level = 0;
+	}
 	
 	public function setLevelNeeded(/*int*/$levelNeeded){
 		$this->_levelNeeded = $levelNeeded;
 	}
-
-	private function __Construct(){
-		$this->_levelNeeded = 1;
-		$this->_session = Session::getInstance();
+	
+	public function test(){
+		if(isset($this->_session['Auth'])){
+			$auth = $this->_session['Auth'];
+			if(isset($auth['id'], $auth['level'])){
+				$this->_id = $auth['id'];
+				$this->_level = $auth['level'];
+			} else {
+				return false;
+			}
+		}		
+		if($this->_level >= $this->_levelNeeded){
+			return true;
+		} else {
+			return false;
+		}
 	}
+	
+	public function getId(){
+		return $this->_id;
+	}
+	
 }
