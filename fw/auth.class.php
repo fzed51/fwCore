f5bd59a5c0ef57d8c4cac27798c61163f7417664
fzed51 <fzed51@yahoo.fr>
2013-11-14 09:58:02 +0100

    Update Auth class

--- fw/auth.class.php
+++ fw/auth.class.php
@@ -4,39 +4,41 @@ class Auth extends Singleton{
 	
 	private $_levelNeeded;
 	private $_session;
-	private $_id;
+	private $_data;
 	private $_level;
+	private $_ipAdresse;
 	
 	private function __Construct(){
 		$this->_levelNeeded = 1;
 		$this->_session = Session::getInstance();
-		$this->_id = 0;
+		$this->_data = array();
 		$this->_level = 0;
+		$this->_ipAdresse = filter_input (INPUT_SERVER,'REMOTE_ADDR', FILTER_VALIDATE_IP);
+		if(isset($this->_session['Auth'])){
+			$auth = $this->_session['Auth'];
+			if(isset($auth['data'], $auth['level'], $auth['ipAdresse'])){
+				$this->_data = $auth['data'];
+				$this->_level = $auth['level'];
+				$this->_ipAdresse = $auth['ipAdresse'];
+			}
+		}
 	}
 	
 	public function setLevelNeeded(/*int*/$levelNeeded){
 		$this->_levelNeeded = $levelNeeded;
 	}
 	
-	public function test(){
-		if(isset($this->_session['Auth'])){
-			$auth = $this->_session['Auth'];
-			if(isset($auth['id'], $auth['level'])){
-				$this->_id = $auth['id'];
-				$this->_level = $auth['level'];
-			} else {
-				return false;
-			}
-		}		
-		if($this->_level >= $this->_levelNeeded){
+	public function test(){		
+		if($this->_ipAdresse == filter_input (INPUT_SERVER,'REMOTE_ADDR', FILTER_VALIDATE_IP) 
+				&& $this->_level >= $this->_levelNeeded){
 			return true;
 		} else {
 			return false;
 		}
 	}
 	
-	public function getId(){
-		return $this->_id;
+	public function getData(){
+		return $this->_data;
 	}
 	
 }
