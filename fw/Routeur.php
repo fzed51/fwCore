31a90b174afa1f072daf12fc580e4ff5a7142587
fzed51 <fzed51@yahoo.fr>
2013-11-14 23:08:58 +0100

    Add routeur class

---
+++ fw/Routeur.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Routeur
+ *
+ * @author Sandrine
+ */
+class Routeur {
+    static public function erreur404 ($message = ''){
+        header('location:');
+    }
+}
