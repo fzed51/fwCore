60c9b59128a68721e426c146ffe3762d2eb4327d
fzed51 <fzed51@yahoo.fr>
2013-11-15 15:11:33 +0100

    Update controleur class

--- fw/controleur.class.php
+++ fw/controleur.class.php
@@ -13,13 +13,46 @@
  */
 class Controleur {
     
-    private $_session;
-    private $_requette;
-    private $_auth;
-    private $_vue;
-    
-    public function executeAction($action){
-        
+    private $data = array();
+	
+	public function __construct() {
+		$this->session = Session::getInstance();
+		$this->auth = Auth::getInstance();
+		$this->requette = null;
+		$this->name = __CLASS__;
+		$this->action = '';
+	}
+	
+	public function __isset($key){
+		return isset($this->data[$key]);
+	}
+	public function __get($key){
+		if ($this->data[$key]){
+			return $this->data[$key];
+		} else {
+			throw new Exception("La propriété '$key' n'existe pas!");
+		}
+		
+	}
+	public function __set($key, $val){
+		$this->data[$key]=$val;
+	}
+
+    public function executeAction(/*string*/$action, Requette $requette){
+		$this->requette = $requette;
+		$action = ucfirst(strtolower($action));
+		$avantAction = 'avant'.$action;
+		if(method_exists($this, 'initControleur')){
+			$this->initControleur();
+		}
+        if(method_exists($this, $avantAction)){
+			$this->$avantAction();
+		}
+		if(method_exists($this, $action)){
+			$this->$action($requette);
+		} else {
+			throw new RuntimeException("L'action '{$this->action}' n'existe pas dans le controleur '{$this->name}'!");
+		}
     }
     
 }
