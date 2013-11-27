e1f108d826c4b0ab5e7b412fb5ac38e4806a2221
fzed51 <fzed51@yahoo.fr>
2013-11-27 14:44:00 +0100

    Add form help class

---
+++ fw/form.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of form
+ *
+ * @author fabien.sanchez
+ */
+class form {
+	
+	public function __construct() {
+		passthru($command);
+	}
+	
+}
