b6e5897c6cd7d555dc5f6363ea33a34d39d82be6
fzed51 <fzed51@yahoo.fr>
2013-12-03 17:32:54 +0100

    Update Vue

--- fw/vue.class.php
+++ fw/vue.class.php
@@ -6,18 +6,54 @@
  * and open the template in the editor.
  */
 
+class VueException extends Exception {
+}
+
+interface iVue{
+	public function generer();
+}
+
 /**
  * Description of vue
  *
  * @author Sandrine
  */
-class vue {
+class Vue implements iVue {
     //put your code here
-	private $html;
-	private $form;
-	
+	protected $html;
+	protected $form;
+	private $_data;
+	private $_script;
+	private $_style;
+			
 	public function __construct() {
 		$this->html = new Html();
 		$this->form = new Form();
+	}
+	
+	public function generer(){
+		
+	}
+	
+	public function set($nom, $donnée = null){
+		if(is_null($donnée)){
+			$this->sets($nom);
+		}else{
+			$this->_data[$nom] = $donnée;
+		}
+	}
+	
+	protected function sets(Array $données){
+		if(is_array($données)){
+			foreach ($données as $key => $value) {
+				$this->set($key, $value);
+			}
+		}else{
+			throw new VueException("Le format des données fournies à la vue ne sont pas valide ! ");
+		}
+	}
+	
+	public function addScript(){
+		
 	}
 }
