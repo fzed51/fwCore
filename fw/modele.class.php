ca05ae198014a9676e66ec28c1880c3e5f1d0e8d
fzed51 <fzed51@yahoo.fr>
2013-12-12 16:32:32 +0100

    Update model class

--- fw/modele.class.php
+++ fw/modele.class.php
@@ -6,3 +6,42 @@
  * and open the template in the editor.
  */
 
+class ModelException extends Exception {
+}
+
+abstract class Model {
+	
+	protected $_table;
+	protected $_config;
+	protected $_pk;
+
+	final protected function setTable($table){
+		$this->_table = $pk;
+		return $this;
+	}
+	final protected function getTable() {
+		return $this->_table;
+	}
+	final protected function setConfig($config) {
+		$this->_config = $pk;
+		return $this;
+	}
+	final protected function getConfig() {
+		return $this->_config;
+	}
+	final protected function setPk($pk) {
+		$this->_pk = $pk;
+		return $this;
+	}
+	final protected function getPk() {
+		return $this->_pk;
+	}
+	
+	public function getById($id){
+	}
+	public function getByFiltre(array $filtre = array()){
+	} 
+	public function getAll(){
+	}
+	
+}
