388861bd8f5fdafd8de56e9cb12bf1b4944ef9b3
fzed51 <fzed51@yahoo.fr>
2013-12-13 11:06:09 +0100

    Update html helper

--- fw/html.class.php
+++ fw/html.class.php
@@ -11,6 +11,7 @@ class Html{
 	
 	private $_stock_css = array();
 	private $_stock_js = array();
+	private $_daraForm = array();
 	
 	public function ctrAction(array $controlAction){
 		$adrCtrlActin = join(WS, $controlAction);
@@ -66,10 +67,19 @@ class Html{
 		return join(' ', $attribut);
 	}
 
+	protected function startElement($tag, $attrbs = array()) { 
+		$attrb = $this->concatAttributs($attrbs);
+		return "<$tag $attrb>";
+	}
+	
+	protected function endElement($tag) {
+		return "</$tag>";
+	}
 
 	protected function element($tag, $attrbs = array(), $content = ''){
-		$attrb = $this->concatAttributs($attrbs);
-		return "<$tag $attrb>$content</$tag>";
+		return $this->startElement($tag, $attrbs) 
+				. $content
+				. $this->endElement($tag);
 	}
 	
 	protected function elementAutoClose($tag, $attrbs = array()){
@@ -92,7 +102,7 @@ class Html{
 			"rel" => '',
 			"media" => '',
 			"size" => ''
-			);
+		);
 		$option = array_merge($defaultOptions, $options);
 		$attributs = array_merge(array('href'=>$href), $option);
 		$link = $this->elementAutoClose('link', $attributs);
@@ -103,12 +113,12 @@ class Html{
 	 * fonction css
 	 * génère un lien avec une feuille de stye Externe;
 	 * 
-	 * @param type $fichier
-	 * @param type $return
-	 * @param type $attributs
-	 * @return null
+	 * @param	string		$fichier
+	 * @param	bool		$return
+	 * @param	array		$attributs
+	 * @return	string/null
 	 */
-	public function css(/*string*/$fichier, $return = true, $attributs = array()){
+	public function css(/*string*/$fichier, /*bool*/$return = true, array $attributs = array()){
 		$defaultAttrbs = array(
 			'rel' => 'stylesheet',
 			'type' => 'text/css',
@@ -117,7 +127,6 @@ class Html{
 		$attributs = array_merge($defaultAttrbs, $attributs);
 		$href = '';
 		if(! filter_var($fichier, FILTER_VALIDATE_URL)){
-			// TODO : détection du fichier css min pour le dev
 			$href = $this->chercheFichier($fichier, '.css');
 		}
 		$link = $this->link($href, $defaultAttrbs);
@@ -129,11 +138,73 @@ class Html{
 		}
 	}
 	
-	public function script(/*string*/$script){
-		
+	public function script(/*string*/$script, array $attributs = array()){
+		$defaultAttrbs = array(
+			'async' => false,
+			'charset' => '',
+			'defer' => false,
+			'src' => '',
+			'type' => 'application/javascript',
+		);
+		$attributs = array_merge($defaultAttrbs, $attributs);
+		$script = $this->startElement('script', $attributs)
+				. $script
+				. $this->endElement('script');
+		return $script;
 	}
 	
-	public function js(/*string*/$script){
-		
+	public function scriptScr(/*string*/$src, array $attributs = array()) {
+		$attributs = array_merge($attributs, array('src' => $src));
+		return $this->script('', $attributs);
+	}
+	
+	public function js(/*string*/$script, /*bool*/$return = true, array $attributs = array()){
+		$defaultAttrbs = array(
+			'async' => false,
+			'charset' => '',
+			'defer' => false,
+			'src' => '',
+			'type' => 'application/javascript',
+		);
+		$attributs = array_merge($defaultAttrbs, $attributs);
+		$script = '';
+		if($return){
+			return $script;
+		}else{
+			$this->_stock_js[] = $script;
+			return null;
+		}
+	}
+	
+	public function startForm($action, array $attributs = array()){
+		$defaultAttrbs = array(
+			'accept-charset' => 'stylesheet',
+					// ISO-8859-1
+					// ISO-8859-15
+					// UTF-8
+			'autocomplete' => '', // on | off
+			'enctype' => 'application/x-www-form-urlencoded',
+					// application/x-www-form-urlencoded
+					// multipart/form-data
+					// text/plain
+			'method' => 'POST', // GET | POST
+			'name' => '',
+			'novalidate' => false,
+			'target' => ''
+					// _blank
+					// _self
+					// _parent
+					// _top
+		);
+		if(isset($attributs['data'])){
+			$this->_daraForm = $attributs['data'];
+			unset($attributs['data']);
+		}
+		$attributs = array_merge($defaultAttrbs, $attributs);
+	}
+	
+	public function endForm(){
+		$this->_daraForm = array();
+		return $this->endElement('form');
 	}
 }
